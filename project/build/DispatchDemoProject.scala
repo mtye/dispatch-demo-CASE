b132c9682f9a0873f3fb178b8a97903e22db2b08
Mark Tye <Mark.Tye@tdameritrade.com>
2010-06-17 15:58:09 -0500

    sbt project and Eclipse classpath changes for Meetup API

--- project/build/DispatchDemoProject.scala
+++ project/build/DispatchDemoProject.scala
@@ -3,6 +3,5 @@ import sbt._
 class DispatchDemoProject(info: ProjectInfo) extends DefaultProject(info) {
   
   val dispatch = "net.databinder" %% "dispatch-http" % "0.7.4"
-
-
+  val meetup = "net.databinder" %% "dispatch-meetup" % "0.7.4"
 }
